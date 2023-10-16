f8b5d674636fa4a559bb85c9ad806091de43e45a
Marco Heyden <heyden@i40pool.ipd.kit.edu>
2023-10-16 17:17:18 +0200

    Added tqdm to async execution

--- util.py
+++ util.py
@@ -100,14 +100,18 @@ def str_to_arr(s, dtype):
     return np.fromstring(s[1:-1], dtype=dtype, sep=" ")
 
 
-def run_async(function, args_list, njobs, sleep_time_s=0.1):
-    pool = Pool(njobs)
-    results = {i: pool.apply_async(function, args=args)
-               for i, args in enumerate(args_list)}
-    while not all(future.ready() for future in results.values()):
-        sleep(sleep_time_s)
-    results = [results[i].get() for i in range(len(results))]
-    pool.close()
+def run_async(function, args_list, njobs, sleep_time_s=0.05):
+    with Pool(njobs) as pool:
+        with tqdm(total=len(args_list)) as pbar:
+            results = {i: pool.apply_async(function, args=args)
+                       for i, args in enumerate(args_list)}
+            previous_state = 0
+            while not all(future.ready() for future in results.values()):
+                this_state = np.sum([future.ready() for future in results.values()])
+                pbar.update(this_state - previous_state)
+                previous_state = this_state
+                sleep(sleep_time_s)
+            results = [results[i].get() for i in range(len(results))]
     return results
 
 
