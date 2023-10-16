6549b4cc5628787e604d3a517c3cd0dc89ab4629
Marco Heyden <heyden@i40pool.ipd.kit.edu>
2023-10-16 17:13:28 +0200

    Fix multiprocessing (resort to parallelizing reps due

--- E_visualize_loss.py
+++ E_visualize_loss.py
@@ -13,40 +13,14 @@ ename = "visualize_loss"
 
 if __name__ == '__main__':
     parameter_choices = {
-        # AdwinK: {"k": [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5],
-        #          "delta": [0.05]}, # in the paper: 1, 5, 10, 20, 30, 40, 50%
-        # WATCH: {
-        #     "kappa": [
-        #         100
-        #     ],
-        #     "mu": [
-        #         1000,
-        #         # 2000
-        #     ],
-        #     "epsilon": [
-        #         # 2,
-        #         3
-        #     ],
-        #     "omega": [
-        #         500,
-        #         # 1000
-        #     ]},
-        # IBDD: {"w": [100, 200, 300],
-        #        "m": [10, 20, 50, 100]},  # no default values in the paper
-        # D3: {"w": [100, 250, 500],
-        #      "roh": [0.1, 0.2, 0.3, 0.4, 0.5],
-        #      "tau": [0.6, 0.7, 0.8, 0.9],
-        #      "model_id": ["lr", "dt"]},  # in the paper: w = [100, 250, 500, 1000, 2500], ρ = [0.1, 0.2, 0.3, 0.4, 0.5] and τ = [0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90]
-        ABCD: {"encoding_factor": [0.3],#, 0.5, 0.7],
+        ABCD: {"encoding_factor": [0.3, 0.5, 0.7],
                "delta": [0.05],
-               "update_epochs": [50], #, 20, 100],
+               "update_epochs": [20, 50, 100],
                "bonferroni": [False],
                "split_type": ["ed"],
                "num_splits": [20],
-               "model_id": ["ae"],#, "pca", "kpca"]
+               "model_id": ["ae", "pca", "kpca"]
                },
-        # IncrementalKS: {"w": [100, 200, 500],
-        #                 "delta": [0.05]},
     }
 
     algorithms = {
@@ -60,21 +34,21 @@ if __name__ == '__main__':
     stretch = True
     n_reps = 1
     datasets = {
-        # GradualHAR: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
-        #               "preprocess": preprocess}],
-        # GradualGasSensors: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
-        #                      "preprocess": preprocess}],
+        GradualHAR: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
+                      "preprocess": preprocess}],
+        GradualGasSensors: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
+                             "preprocess": preprocess}],
         GradualLED: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
                       "preprocess": preprocess, "n_per_concept": n_per_concept}],
-        # GradualRBF: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
-        #               "preprocess": preprocess, "n_per_concept": n_per_concept,
-        #               "dims": dims, "add_dims_without_drift": True}],
+        GradualRBF: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
+                      "preprocess": preprocess, "n_per_concept": n_per_concept,
+                      "dims": dims, "add_dims_without_drift": True}],
         GradualMNIST: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
                         "preprocess": preprocess, "n_per_concept": n_per_concept}],
-        # GradualFashionMNIST: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
-        #                        "preprocess": preprocess, "n_per_concept": n_per_concept}],
-        # GradualCifar10: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
-        #                   "preprocess": preprocess, "n_per_concept": n_per_concept}],
+        GradualFashionMNIST: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
+                               "preprocess": preprocess, "n_per_concept": n_per_concept}],
+        GradualCifar10: [{"num_concepts": n_concepts, "drift_length": drift_length, "stretch": stretch,
+                          "preprocess": preprocess, "n_per_concept": n_per_concept}],
     }
 
     experiment = Experiment(name=ename, configurations=algorithms, datasets=datasets,
